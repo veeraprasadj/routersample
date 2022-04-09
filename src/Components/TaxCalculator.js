475923db58ae273d5e681ea1ade73c7b6c005f66
Prasad <veeraprasad.j@gmail.com>
2022-04-09 12:39:18 +0100

    Formik

--- src/Components/TaxCalculator.js
+++ src/Components/TaxCalculator.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function TaxCalculator() {
+  const initialValues = {
+    principleAmount: 0,
+    ir: 0,
+    tenure: 0,
+  };
+
+  const validationSchema = Yup.object({
+    principleAmount: Yup.number().required("Required"),
+    ir: Yup.number().required("Interest rate is required"),
+    tenure: Yup.number().required("Tenure is required"),
+  });
+
+  const onSubmit = (values) => {
+    console.log("Form data", values);
+  };
+
   return (
-    <div>TaxCalculator</div>
-  )
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <div>TaxCalculator</div>
+        <div className="form-group">
+          <label htmlFor="principleAmout">PrincipleAmount</label>
+          <Field
+            type="number"
+            name="principleAmount"
+            id="principleAmount"
+            className="form-control"
+          ></Field>
+          <ErrorMessage name="number" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="ir">Interest Rate</label>
+          <Field
+            type="number"
+            name="ir"
+            id="ir"
+            className="form-control"
+          ></Field>
+          <ErrorMessage name="ir" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="tenure">Tenure in years</label>
+          <Field
+            type="number"
+            name="tenure"
+            id="tenure"
+            className="form-control"
+          ></Field>
+          <ErrorMessage name="tenure" />
+        </div>
+        <div>
+          <input type="submit">Calculate</input>
+        </div>
+      </Form>
+    </Formik>
+  );
 }
 
-export default TaxCalculator
+export default TaxCalculator;
